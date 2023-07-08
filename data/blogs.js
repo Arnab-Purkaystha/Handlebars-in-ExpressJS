40324ce32191f6b7bbce5b836de584bb7b3fec03
ARNAB PURKAYSTHA <80410084+Arnab-Purkaystha@users.noreply.github.com>
2023-07-08 20:00:54 +0530

    Add files via upload

---
+++ data/blogs.js
@@ -0,0 +1,21 @@
+blogs =
+    [
+        {
+            title: "How to get started with C++",
+            content: "This is Content",
+            slug: "c++ learn"
+        },
+        {
+            title: "How to get started with C#",
+            content: "This is Content",
+            slug: "c# learn"
+        },
+        {
+            title: "How to get started with Java",
+            content: "This is Content",
+            slug: "Java learn"
+        },
+    ]
+
+
+module.exports = blogs;
